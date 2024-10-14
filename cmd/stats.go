c89950b97a4336b8dc51333abd69f57e24e1f493
siddhant <siddhant.pandey@middleware.io>
2024-10-14 18:34:27 +0530

    wrapped up the implementation of stats module

--- cmd/stats.go
+++ cmd/stats.go
@@ -182,3 +182,74 @@ func printCells(cols map[int]column) {
 		fmt.Printf("\n")
 	}
 }
+
+
+func printMonths() {
+	week := getBeginningOfDay(time.Now()).Add(-(time.Duration(daysIn6Months) * time.Hour * 24))
+	month := week.Month()
+	fmt.Printf("         ")
+	for {
+		if week.Month() != month {
+			fmt.Printf("%s ", week.Month().String()[:3])
+			month = week.Month()
+		} else {
+			fmt.Printf("    ")
+		}
+
+		week = week.Add(7 * time.Hour * 24)
+		if week.After(time.Now()) {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+
+func printDayCols(day int) {
+	out := "     "
+	switch day {
+	case 1:
+		out = " Mon "
+	case 3:
+		out = " Wed "
+	case 5:
+		out = " Fri "
+	}
+
+	fmt.Printf(out)
+}
+
+
+
+func printCell(val int, today bool) {
+    escape := "\033[0;37;30m"
+    switch {
+    case val > 0 && val < 5:
+        escape = "\033[1;30;47m"
+    case val >= 5 && val < 10:
+        escape = "\033[1;30;43m"
+    case val >= 10:
+        escape = "\033[1;30;42m"
+    }
+
+    if today {
+        escape = "\033[1;37;45m"
+    }
+
+    if val == 0 {
+        fmt.Printf(escape + "  - " + "\033[0m")
+        return
+    }
+
+    str := "  %d "
+    switch {
+    case val >= 10:
+        str = " %d "
+    case val >= 100:
+        str = "%d "
+    }
+
+    fmt.Printf(escape+str+"\033[0m", val)
+}
+
+
